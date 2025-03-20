1f30b6e83564e5a24127597ace5286e4ad908131
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2025-03-21 00:14:46 +0200

    ADDED: HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import css from "./HomePage.module.css";
+const HomePage = () => {
+ 
+     const navigate = useNavigate();
+
+    const handleViewNowClick = () => {
+        navigate('/catalog');
+    };
+
+  return (
+    
+     <div className={css.container}>
+      <h1 className={css.title}>Campers of your dreams</h1>
+      <p className={css.paragraph} >You can find everything you want in our catalog</p>
+        <button className={css.heroBtn} onClick={handleViewNowClick}>View now</button>
+        </div>
+      
+    
+  );
+};
+export default HomePage;
