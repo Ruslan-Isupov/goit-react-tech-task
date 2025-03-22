7170a5c112dd8329f95a954a633c6ae14a986470
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2025-03-22 17:11:05 +0200

    ADDED: camperOps

---
+++ src/redux/camperOps.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+
+export const getAllCampers = createAsyncThunk(
+  "campers/getAll",
+  async (filters = {}, thunkAPI) => {
+    try {
+      const params = { ...filters };
+      const response = await axios.get("/campers", { params });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getCamperById = createAsyncThunk(
+  "campers/getById",
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.get(`/campers/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
