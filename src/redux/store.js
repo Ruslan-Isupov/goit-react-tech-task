1cde240e00d2b4a0296ce87f05d5d3e30dc7ef6f
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2025-03-22 17:22:33 +0200

    ADDED: store

---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import trucksReducer from "./trucksSlice";
+import sessionStorage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage: sessionStorage,
+  whitelist: ["favorites"],
+};
+
+const rootReducer = combineReducers({
+  trucks: trucksReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
