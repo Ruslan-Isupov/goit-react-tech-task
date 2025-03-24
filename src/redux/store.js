1a8d4ba04237944f2cd34d07cbc56b335da0925f
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2025-03-25 01:41:21 +0200

    DELETED: store comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,3 @@
-// import { combineReducers, configureStore } from "@reduxjs/toolkit";
-// import { trucksReducer } from "./trucksSlice";
-// import { filtersReducer } from "./filtersSlice";
-// import { favoritesReducer } from "./favoritesSlice";
-// import sessionStorage from "redux-persist/lib/storage";
-// import { persistStore, persistReducer } from "redux-persist";
-
-// const persistConfig = {
-//   key: "root",
-//   storage: sessionStorage,
-//   whitelist: ["favorites"],
-// };
-
-// const rootReducer = combineReducers({
-//   trucks: trucksReducer,
-//   filters: filtersReducer,
-//   favorites: favoritesReducer,
-// });
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-// });
-
-// export const persistor = persistStore(store);
 import { configureStore } from "@reduxjs/toolkit";
 import { trucksReducer } from "./trucksSlice";
 import { filtersReducer } from "./filtersSlice";
